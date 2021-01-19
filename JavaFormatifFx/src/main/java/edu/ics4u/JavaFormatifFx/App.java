8339b9e61332cd86075121cbffa6ea690f833e2c
wesle <wesle@192.168.0.19>
2021-01-19 15:46:07 -0500

    Second commit

--- JavaFormatifFx/src/main/java/edu/ics4u/JavaFormatifFx/App.java
+++ JavaFormatifFx/src/main/java/edu/ics4u/JavaFormatifFx/App.java
@@ -2,28 +2,31 @@ package edu.ics4u.JavaFormatifFx;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-
-/**
- * JavaFX App
- */
 public class App extends Application {
 
-    @Override
-    public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+    public void start(Stage stage) 
+    {
+ 
+        var label = new Label("Discord");
+        var text = new TextField("Type here"); 
+        var button = new Button("Push to talk"); 
+        
+        var hbox = new HBox(text, button);   
+        var root = new VBox(label, hbox); 
+        var scene = new Scene(root, 450, 400); 
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         launch();
     }
 
